52d87e6ce3dbd8a200df1d6d119f6f0185a35419
Your Name <you@example.com>
2021-09-25 13:24:21 +0530

    modified alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,12 +2,13 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType) => {
+  const id = uuid;
   return {
     type: SET_ALERT,
     payload: {
       msg,
       alertType,
-      id: uuid,
+      id,
     },
   };
 };
